6bb28e1cac7423b9061b6f70bca6dec2ed831b36
arcarver <acarver@byupathway.edu>
2025-09-11 13:42:43 -0700

    new folder

---
+++ week2/makeJson.js
@@ -0,0 +1,13 @@
+conts wardObject1 = {
+    familyName: "Carver",
+    moveInDate: "Aug 31, 2007",
+    familySize: 7,
+    visited: true;
+    individualData: {
+        firstName: "Annie",
+        gender: "female",
+        birthdate: "Nov 29, 1979",
+    }
+};
+
+const jfamilyJsonString = JSON.stringify(wardObject1);
