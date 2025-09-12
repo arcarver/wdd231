06007492c6e623c4a1140e6b196cdae65e6a44b6
arcarver <acarver@byupathway.edu>
2025-09-11 22:17:45 -0700

    formatting

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -19,7 +19,7 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
+        portrait.setAttribute('max-width', '340');
         portrait.setAttribute('height', '440');
 
         card.appendChild(fullname);
