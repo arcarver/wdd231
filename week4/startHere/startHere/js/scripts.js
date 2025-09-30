508f85213ddceb10922a6c942914f70b17d83050
arcarver <acarver@byupathway.edu>
2025-09-29 17:44:48 -0700

    inital set up of join

---
+++ week4/startHere/startHere/js/scripts.js
@@ -0,0 +1,10 @@
+const myInfo = new URLSearchParams(window.location.search);
+console.log(myInfo);
+
+console.log(myInfo.get("first"));
+
+document.querySelector("#results").innerHTML = `
+<p>Appointment for ${myInfo.get("first")} ${myInfo.get("last")}</p>
+<p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} Temple</p>
+<p>Your phone is ${myInfo.get('phone')}</p>
+<p>Your email is ${myInfo.get('email')}</p>`
