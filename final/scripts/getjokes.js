a5feabaff80fab32483347e15c65224ac7e27a9d
arcarver <acarver@byupathway.edu>
2025-10-12 20:23:52 -0700

    fetch works

---
+++ final/scripts/getjokes.js
@@ -0,0 +1,37 @@
+const randomJoke = document.querySelector("#randomJoke");
+
+// fetch('GET https://icanhazdadjoke.com/')
+// .then(response => {
+//     if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     return response.json();
+// })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('There was a problem with the fetch operation. Try again later', error);
+//     });
+
+async function fetchJoke() {
+    try {
+        const response = await fetch(
+            'https://icanhazdadjoke.com/',
+            {
+                headers: { "Accept": "application/json" },
+            }
+        );
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        console.log(data.joke);
+    } catch (error) {
+        console.error('Error fetching joke:', error);
+    }
+}
+
+fetchJoke();
+// GET https://icanhazdadjoke.com/
