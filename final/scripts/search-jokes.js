d9d7433507a6d80657ca98892e7e5370e6310a5c
arcarver <acarver@byupathway.edu>
2025-10-13 21:59:34 -0700

    formatting getting jokes not yet working

--- final/scripts/search-jokes.js
+++ final/scripts/search-jokes.js
@@ -20,6 +20,21 @@ async function fetchJokes() {
         console.error('Error fetching joke:', error);
     }
 }
+
+const myInfo = new URLSearchParams(window.location.search);
+console.log(myInfo);
+
+console.log(myInfo.get("first"));
+
+document.querySelector("#results").innerHTML = `
+<p>Membership for ${myInfo.get("first")} ${myInfo.get("last")}</p>
+<p>Membership level is  ${myInfo.get('level')} on ${myInfo.get('timestamp')} </p>
+<p>Your phone is ${myInfo.get('phone')}</p>
+<p>Your email is ${myInfo.get('email')}</p>
+<p>You are a ${myInfo.get('title')} at ${myInfo.get('organization')}</p>
+<p>Your email is ${myInfo.get('email')}</p>
+<p>${myInfo.get('description')}`
+
 // searchJoke.addEventListener('input', function (event) {
 //     console.log('hi', event.target.value);
 // })
